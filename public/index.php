c5364b9c324845e26a4d2aa63d7a0b1f7ecf4886
Sergey Petrenko <spetrenko@me.com>
2015-12-11 10:35:31 +0300

    566a2ec7f6325d8a08d847e3 Take out from Remark

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
-$tem
+require_once($_SERVER["DOCUMENT_ROOT"] . "/../app/engine/views/header.html");
+if(isset($_GET["page"])) {
+  require_once($_SERVER["DOCUMENT_ROOT"] . "/pages/".$_GET["page"].".html");
+}
+require_once($_SERVER["DOCUMENT_ROOT"] . "/../app/engine/views/footer.html");
 
 //require_once ( $_SERVER["DOCUMENT_ROOT"] . "/../app/engine/load.php");
