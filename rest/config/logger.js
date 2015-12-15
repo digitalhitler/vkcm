074b4141a9437b923396d948cbd07021d0cf66b7
Getrix <me@digitalhitler.ru>
2015-12-15 09:55:03 +0300

    @trello Frontend principles
    CSS fw philosophy changes.
    WebStorm settings archive
    
    @taskid 566a28940b1784dead1ec2f3
    @number 11

---
+++ rest/config/logger.js
@@ -0,0 +1,54 @@
+var fs = require('fs');
+var cluster = require('cluster');
+
+exports.default = { 
+  logger: function(api){
+    var logger = { transports: [] };
+
+    // console logger
+    if(cluster.isMaster){
+      logger.transports.push(function(api, winston){
+        return new (winston.transports.Console)({
+          colorize: true,
+          level: 'info',
+          timestamp: api.utils.sqlDateTime
+        });
+      });
+    }
+
+    // file logger
+    try{
+      fs.mkdirSync('./log');
+    } catch(e) {
+      if(e.code !== 'EEXIST'){
+        return next([new Error('Cannot create ./log directory'), e])
+      }
+    }
+    logger.transports.push(function(api, winston) {
+      return new (winston.transports.File)({
+        filename: api.config.general.paths.log[0] + '/' + api.pids.title + '.log',
+        level: 'info',
+        timestamp: true
+      });
+    });
+
+    // the maximum length of param to log (we will truncate)
+    logger.maxLogStringLength = 100;
+
+    // you can optionally set custom log levels 
+    // logger.levels = {good: 0, bad: 1};
+
+    // you can optionally set custom log colors 
+    // logger.colors = {good: 'blue', bad: 'red'};
+    
+    return logger;
+  }
+}
+
+exports.test = { 
+  logger: function(api){
+    return {
+      transports: null
+    }
+  }
+}
